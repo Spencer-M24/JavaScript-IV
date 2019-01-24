b8e26c4c13de71a26e42cf69414276f9fbce4802
Spencer Meredith <Smeredith1119@gmail.com>
2019-01-24 15:47:33 -0800

    working on lambda classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,67 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(person) {
+
+        this.name = person.name;
+        this.age = person.age;
+        this.location = person.location;
+        this.gender = person.gender;
+
+    }
+    speak(){
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`)
+    }
+    }
+
+    class Instructor extends Person {
+        constructor(instructor){
+            super(instructor);
+            this.favLanguage = instructor.favLanguage;
+            this.specialty= instructor.specialty;
+            this.catchPhrase= instructor.catchPhrase;
+
+        
+    }
+
+    demo(subject){
+        console.log(`Make sure you are going to study ${homework}`);
+    }
+    grade(student, subject){
+        console.log(`${child.name} show something here
+    ${homework}`)
+    }
+
+    class Student extends Person {
+        constructor(students){
+            super(students);
+            this.food = students.food;
+            this.teach= students.teach;
+            this.show = students.show;
+            this.letstry= students.letstry;
+            this.className = students.className;
+            this.fun = students.fun;
+        }
+    }
+
+    wholeThing(){
+        console.log(`${this.name}` this is the whole 
+
+    }
+
+
+    class ProjectManager extends Instructor{
+        constructor(pm)
+        super(pm);
+        this.gradClassName = pm.gradClassName;
+        this.favInstructor = pm.favInstructor;
+
+    }
+
+    standUp(channel){
+        console.log(`${this.name}` will become ${rats},);
+
+    }
+
+    debugcode(student, student){
+        console.log (`${this.name} fix ${})
+    }
